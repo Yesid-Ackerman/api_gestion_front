6a972d01d7c66c4272ce6fb6365ddfa8197aaf5f
Yesid Ackerman <deironyesithym21@gmail.com>
2024-10-15 17:58:00 -0500

    Login Interface Incompleted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { LoginComponent } from '@pages/login/login.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: "transactions",
     // component: TransactionsComponent,
     loadComponent:()=>import('@pages/transactions/transactions.component').then(c=> c.TransactionsComponent)
+  },
+
+  {
+    path: "login",
+    component: LoginComponent,
   }
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+        'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      },
+      animation: {
+        'slow-spin': 'spin 10s linear infinite', // Ajusta la duración a 5 segundos
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
